c8d474dba57a8fe8c7c37cc6e22f9fd62eeb3de9
gellen <hellogellen@gmail.com>
2021-05-22 01:39:21 +0700

    update server.py

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 # Import the modules
 from flask import Flask, request, jsonify, make_response
-from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.xception import preprocess_input
 from tensorflow.keras.models import load_model
-import cv2
 import numpy as np
 import uuid
+import subprocess
 from flask_cors import CORS, cross_origin
 
 # Model label
-names = ["Adas", "Akar Alang Alang", "Bawang Putih", "Daun Katuk", "Daun Salam", "Jahe", "Kapulaga", "Kumis Kucing", "Kunyit", "Lidah Buaya", "Mengkudu", "Meniran", "Miana", "Pare", "Pegagan", "Rosela", "Seledri", "Serai", "Temu Kunci", "Valerian"]
+labels = ["Adas", "Akar Alang Alang", "Bawang Putih", "Daun Katuk", "Daun Salam", "Jahe", "Kapulaga", "Kumis Kucing", "Kunyit", "Lidah Buaya", "Mengkudu", "Meniran", "Miana", "Pare", "Pegagan", "Rosela", "Seledri", "Serai", "Temu Kunci", "Valerian"]
 
 # Process image and predict label
 def predict_image(IMG_PATH):
@@ -16,21 +17,22 @@ def predict_image(IMG_PATH):
     model = load_model("model_xception_herbify.h5")
     
     # Preprocess the image
-    image = cv2.imread(IMG_PATH)
-    image = cv2.resize(image, (199, 199))
-    image = image.astype("float") / 255.0
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
+    img = image.load_img(IMG_PATH, target_size=(299, 299))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
 
     # Predict the image
-    res = model.predict(image)
-    label = np.argmax(res)
+    images = np.vstack([x])
+    proba = model.predict(images)[0]
 
-    # Print out the label
-    print("Label", label)
-    labelName = names[label]
-    print("Label name:", labelName)
-    return labelName
+    # Create dict to save the results
+    results = {}
+    for (label, p) in zip(labels, proba):
+        # Append the result to dictionary
+        results[label] = "{:.2f}%".format(p * 100)
+
+    return results
 
 
 # Initializing flask application
@@ -46,10 +48,6 @@ def main():
 # Endpoint for upload the image
 @app.route("/predict", methods=["POST"])
 def process_req():
-    # Get the posted data
-    data = request.get_json(force=True)
-    uid = data['uid']
-
     # Get image
     img = request.files["img"]
 
@@ -60,13 +58,11 @@ def process_req():
     # Predict the image
     predict_result = predict_image(img_file)
 
-    response = {
-        'uid': uid,
-        'predictResult': predict_result
-    }
+    # Move image to cloud storage
+    subprocess.run(["gsutil", "mv", img_file, "gs://herbify-testing/uploads/"])
 
     # Return response
-    return make_response(jsonify(response), 200)
+    return make_response(jsonify(predict_result), 200)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
