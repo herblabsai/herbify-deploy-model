0ec34a75a650d95946e52015a4e74daadedbd381
gellen <hellogellen@gmail.com>
2021-05-30 21:05:57 +0700

    Update server.py

--- server.py
+++ server.py
@@ -9,13 +9,16 @@ import subprocess
 from flask_cors import CORS, cross_origin
 
 # Model label
-labels = ["Adas", "Akar Alang Alang", "Bawang Putih", "Daun Katuk", "Daun Salam", "Jahe", "Kapulaga", "Kumis Kucing", "Kunyit", "Lidah Buaya", "Mengkudu", "Meniran", "Miana", "Pare", "Pegagan", "Rosela", "Seledri", "Serai", "Temu Kunci", "Valerian"]
+labels = ["Adas", "Akar Alang Alang", "Bawang Putih", "Daun Katuk", "Daun Salam", "Jahe", "Kapulaga", "Kumis Kucing", "Kunyit",
+          "Lidah Buaya", "Mengkudu", "Meniran", "Miana", "Pare", "Pegagan", "Rosela", "Seledri", "Serai", "Temu Kunci", "Valerian"]
 
 # Process image and predict label
+
+
 def predict_image(IMG_PATH):
     # Load the model
     model = load_model("model_xception_herbify.h5")
-    
+
     # Preprocess the image
     img = image.load_img(IMG_PATH, target_size=(299, 299))
     x = image.img_to_array(img)
@@ -26,11 +29,15 @@ def predict_image(IMG_PATH):
     images = np.vstack([x])
     proba = model.predict(images)[0]
 
+    # Cloud Storage URL for storing placeholder images
+    URL = "https://storage.googleapis.com/herbify/herb-placeholder/"
+
     # Create dict to save the results
     arr_data = []
     for (label, p) in zip(labels, proba):
         # Append the result to arr_data
-        result = dict({"nama_tumbuhan": label, "score": float("{:.2f}".format(p * 100))})
+        result = dict(
+            {"name": label, "confident": float("{:.2f}".format(p * 100)), "imageUrl": URL + label + ".jpg"})
         arr_data.append(result)
 
     return {"data": arr_data}
@@ -40,6 +47,7 @@ def predict_image(IMG_PATH):
 app = Flask(__name__)
 cors = CORS(app)
 
+
 @app.route("/")
 def main():
     return """
@@ -47,6 +55,8 @@ def main():
     """
 
 # Endpoint for upload the image
+
+
 @app.route("/predict", methods=["POST"])
 def process_req():
     # Get image
@@ -60,10 +70,11 @@ def process_req():
     predict_result = predict_image(img_file)
 
     # Move image to cloud storage
-    subprocess.run(["gsutil", "mv", img_file, "gs://herbify-testing/uploads/"])
+    subprocess.run(["gsutil", "mv", img_file, "gs://herbify/upload-images/"])
 
     # Return response
     return make_response(jsonify(predict_result), 200)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
